223bed71caa94160f02f4a4c6209d6526f47f719
Ravi-047 <ravi047raj@gmail.com>
2022-11-09 13:59:11 +0530

    __Login__

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+// import Register from './Static-Pages/RegisterPage/Register';
+import Login from './Static-Pages/LoginPage/Login';
 
 function App() {
   return (
-    <div className="App">
-     <h1>App is ready to start</h1>
-     
+    <div>
+      <h1>App is ready to start</h1>
+      <Login />
+      {/* <Register /> */}
     </div>
   );
 }
---
+++ frontend/src/Static-Pages/RegisterPage/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Register.module.css";
+
+const Register = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Register;
