2d0250a9078a61d10a50a3e042e8aeb159a975ff
Nandlal <shahnandlal49@gmail.com>
2022-12-24 15:48:21 +0530

    User cannot go to seller or admin seller cannot visit admin and vice versa functionality added

--- frontend/src/AllRoutes.jsx
+++ frontend/src/AllRoutes.jsx
@@ -1,72 +1,96 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import HomePage from './Static-Pages/HomePage/HomePage'
-import Brands from './Static-Pages/Brands/Brands'
-import Holiday from './Static-Pages/Holiday/Holiday'
-import SkinCare from './Static-Pages/SkinCare/SkinCare'
-import Hair from './Static-Pages/Hair/Hair'
-import Makeup from './Static-Pages/Makeup/Makeup'
-import Bath from './Static-Pages/Bath-Body/Bath'
-import Fragrance from './Static-Pages/Fragrance/Fragrance'
-import Selfcare from './Static-Pages/Selfcare/Selfcare'
-import Tools from './Static-Pages/Tools/Tools'
-import Sales from './Static-Pages/SalesPage/Sales'
-import Login from './Static-Pages/LoginPage/Login'
-import Register from './Static-Pages/RegisterPage/Register'
-import Carts from './Static-Pages/Cart/Carts'
-import Payment from './Payment-Page/Payment'
-import ProductDetails from './Static-Pages/ProductsSpecifications/ProductDetails'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Static-Pages/HomePage/HomePage";
+import Brands from "./Static-Pages/Brands/Brands";
+import Holiday from "./Static-Pages/Holiday/Holiday";
+import SkinCare from "./Static-Pages/SkinCare/SkinCare";
+import Hair from "./Static-Pages/Hair/Hair";
+import Makeup from "./Static-Pages/Makeup/Makeup";
+import Bath from "./Static-Pages/Bath-Body/Bath";
+import Fragrance from "./Static-Pages/Fragrance/Fragrance";
+import Selfcare from "./Static-Pages/Selfcare/Selfcare";
+import Tools from "./Static-Pages/Tools/Tools";
+import Sales from "./Static-Pages/SalesPage/Sales";
+import Login from "./Static-Pages/LoginPage/Login";
+import Register from "./Static-Pages/RegisterPage/Register";
+import Carts from "./Static-Pages/Cart/Carts";
+import Payment from "./Payment-Page/Payment";
+import ProductDetails from "./Static-Pages/ProductsSpecifications/ProductDetails";
 
-import { UserInfo } from './Static-Pages/UserInfo/UserInfo'
+import { UserInfo } from "./Static-Pages/UserInfo/UserInfo";
 
-import AddProduct from "./Static-Pages/seller/AddProduct"
+import AddProduct from "./Static-Pages/seller/AddProduct";
 
-import Privateroute from './Components/Privateroute'
+import Privateroute from "./Components/Privateroute";
+import AdminPrivate from "./Components/AdminPrivate";
+import SellerPrivate from "./Components/SellerPrivate";
 
 function AllRoutes() {
   return (
     <Routes>
-<Route path='/' element={<HomePage/>}/>
-<Route path='/Brands' element={<Brands/>}/>
-<Route path='/Holiday' element={<Holiday/>}/>
-<Route path='/Sale' element={<Sales/>}/>
-<Route path='/Register' element={<Register/>}/>
-{/* add proucts page link here  */}
-<Route path='/Login' element={<Login/>}/>
-<Route path='/SkinCare' element={<SkinCare/>}/>
-<Route path='/Hair' element={<Hair/>}/>
-<Route path='/Makeup' element={<Makeup/>}/>
-<Route path='/BathBody' element={<Bath/>}/>
-<Route path='/Fragrance' element={<Fragrance/>}/>
-<Route path='/SelfCare' element={<Selfcare/>}/>
-<Route path='/Tools' element={<Tools/>}/>
-<Route path='/Sale/:id/Carts' element={
-<Privateroute>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/Brands" element={<Brands />} />
+      <Route path="/Holiday" element={<Holiday />} />
+      <Route path="/Sale" element={<Sales />} />
+      <Route path="/Register" element={<Register />} />
+      {/* add proucts page link here  */}
+      <Route path="/Login" element={<Login />} />
+      <Route path="/SkinCare" element={<SkinCare />} />
+      <Route path="/Hair" element={<Hair />} />
+      <Route path="/Makeup" element={<Makeup />} />
+      <Route path="/BathBody" element={<Bath />} />
+      <Route path="/Fragrance" element={<Fragrance />} />
+      <Route path="/SelfCare" element={<Selfcare />} />
+      <Route
+        path="/Tools"
+        element={
+          // Admin private route is implemented here please make admin page wrap this route
+          <AdminPrivate>
+            <Tools />
+          </AdminPrivate>
+        }
+      />
+      <Route
+        path="/Sale/:id/Carts"
+        element={
+          <Privateroute>
+            <Carts />
+          </Privateroute>
+        }
+      />
+      <Route
+        path="/Payment"
+        element={
+          <Privateroute>
+            <Payment />
+          </Privateroute>
+        }
+      />
+      <Route path="/Sale/:id" element={<ProductDetails />} />
 
-<Carts/>
-</Privateroute>
+      <Route
+        path="/userinfo"
+        element={
+          <Privateroute>
+            <UserInfo />
+          </Privateroute>
+        }
+      />
 
-}/>
-<Route path='/Payment' element={
-<Privateroute>
+      <Route
+        path="/addproduct"
+        element={
+          <Privateroute>
+            <SellerPrivate>
+              <AddProduct />
+            </SellerPrivate>
+          </Privateroute>
+        }
+      />
 
-<Payment/>
-</Privateroute>
-
-}/>
-<Route path='/Sale/:id' element={<ProductDetails/>}/>
-
-<Route path='/userinfo' element={<UserInfo />} />
-
-<Route path='/addproduct' element={<AddProduct/>}/>
-
-
-{/* <Route path='/Carts' element={<Carts/>}/> */}
-
-
-
+      {/* <Route path='/Carts' element={<Carts/>}/> */}
     </Routes>
-  )
+  );
 }
 
-export default AllRoutes
+export default AllRoutes;
