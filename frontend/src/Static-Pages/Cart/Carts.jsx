d1db8f4922a74cd9e027d4eb823c2f4f8695d417
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2022-12-24 14:26:32 +0530

    change cart page style

--- frontend/src/Static-Pages/Cart/Carts.jsx
+++ frontend/src/Static-Pages/Cart/Carts.jsx
@@ -79,20 +79,19 @@ const Carts = () => {
         </div>           
       </MDBCol>
     </MDBRow>
-    <MDBCard className="rounded-3 mb-4">
+    {
+      data.map((el)=>(
+        <MDBCard className="rounded-3 mb-4">
           <MDBCardBody className="p-4">
             <MDBRow className="justify-content-between align-items-center">
               <MDBCol md="2" lg="2" xl="2">
                 <MDBCardImage className="rounded-3" fluid
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
+                  src={el.image}
                   alt="Cotton T-shirt" />
               </MDBCol>
               <MDBCol md="3" lg="3" xl="3">
-                <p className="lead fw-normal mb-2">Basic T-shirt</p>
-                <p>
-                  <span className="text-muted">Size: </span>M{" "}
-                  <span className="text-muted">Color: </span>Grey
-                </p>
+                <p className="lead fw-normal mb-2">{el.title}</p>
+                
               </MDBCol>
               <MDBCol md="3" lg="3" xl="2"
                 className="d-flex align-items-center justify-content-around">
@@ -100,15 +99,14 @@ const Carts = () => {
                   <MDBIcon fas icon="minus" />
                 </MDBBtn>
 
-                <MDBInput min={0} defaultValue={2} type="number" size="sm" />
-
+                {el.quantity}
                 <MDBBtn color="link" className="px-2">
                   <MDBIcon fas icon="plus" />
                 </MDBBtn>
               </MDBCol>
               <MDBCol md="3" lg="2" xl="2" className="offset-lg-1">
                 <MDBTypography tag="h5" className="mb-0">
-                  $499.00
+                  ${el.price}
                 </MDBTypography>
               </MDBCol>
               <MDBCol md="1" lg="1" xl="1" className="text-end">
@@ -119,6 +117,9 @@ const Carts = () => {
             </MDBRow>
           </MDBCardBody>
         </MDBCard>
+      ))
+    }
+    
 
   </MDBContainer>
 </section>
